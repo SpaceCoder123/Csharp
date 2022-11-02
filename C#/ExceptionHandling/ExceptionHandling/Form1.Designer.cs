89c53e8fbe0bee04f130f74ddbb66c3372116099
Mohan Ram T.R <51279552+SpaceCoder123@users.noreply.github.com>
2022-11-02 19:16:04 +0530

    Add files via upload

---
+++ C#/ExceptionHandling/ExceptionHandling/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ExceptionHandling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CalculateBtn = new System.Windows.Forms.Button();
+            this.Num2Inp = new System.Windows.Forms.TextBox();
+            this.Num1Inp = new System.Windows.Forms.TextBox();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.OutputLabel1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CalculateBtn
+            // 
+            this.CalculateBtn.Location = new System.Drawing.Point(241, 136);
+            this.CalculateBtn.Name = "CalculateBtn";
+            this.CalculateBtn.Size = new System.Drawing.Size(123, 45);
+            this.CalculateBtn.TabIndex = 0;
+            this.CalculateBtn.Text = "Calculate";
+            this.CalculateBtn.UseVisualStyleBackColor = true;
+            this.CalculateBtn.Click += new System.EventHandler(this.CalculateBtn_Click);
+            // 
+            // Num2Inp
+            // 
+            this.Num2Inp.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.Num2Inp.Location = new System.Drawing.Point(52, 163);
+            this.Num2Inp.Name = "Num2Inp";
+            this.Num2Inp.Size = new System.Drawing.Size(100, 45);
+            this.Num2Inp.TabIndex = 1;
+            // 
+            // Num1Inp
+            // 
+            this.Num1Inp.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.Num1Inp.Location = new System.Drawing.Point(52, 108);
+            this.Num1Inp.Name = "Num1Inp";
+            this.Num1Inp.Size = new System.Drawing.Size(100, 45);
+            this.Num1Inp.TabIndex = 2;
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.AutoSize = true;
+            this.OutputLabel.Location = new System.Drawing.Point(562, 142);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(0, 16);
+            this.OutputLabel.TabIndex = 3;
+            // 
+            // OutputLabel1
+            // 
+            this.OutputLabel1.AutoSize = true;
+            this.OutputLabel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.OutputLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.OutputLabel1.Location = new System.Drawing.Point(409, 142);
+            this.OutputLabel1.Name = "OutputLabel1";
+            this.OutputLabel1.Size = new System.Drawing.Size(0, 39);
+            this.OutputLabel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 505);
+            this.Controls.Add(this.OutputLabel1);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.Num1Inp);
+            this.Controls.Add(this.Num2Inp);
+            this.Controls.Add(this.CalculateBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CalculateBtn;
+        private System.Windows.Forms.TextBox Num2Inp;
+        private System.Windows.Forms.TextBox Num1Inp;
+        private System.Windows.Forms.Label OutputLabel;
+        private System.Windows.Forms.Label OutputLabel1;
+    }
+}
+
---
+++ C#/ExceptionHandling/ExceptionHandling/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExceptionHandling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateBtn_Click(object sender, EventArgs e)
+        {
+ 
+            try
+            {
+                double output = Convert.ToInt32(Num1Inp.Text) / Convert.ToInt32(Num2Inp.Text);
+                OutputLabel1.Text += Convert.ToString(output);
+            }
+            catch (DivideByZeroException ex)
+            {
+                //throw new Exception("Cannot divide the number by 0");
+                MessageBox.Show(ex.ToString());
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                MessageBox.Show("The program has been finished! ");2
+                Application.Exit();
+            }
+            
+        }
+    }
+}
